875868294d0dc1ffb0a91f8ea7ff988a28ba9393
vkpati20 <vkpati20@gmail.com>
2020-09-06 17:32:23 -0500

    Added images

--- Solution.py
+++ Solution.py
@@ -8,10 +8,9 @@ Created on Sat Sep  5 23:50:54 2020
 
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
+from scipy.spatial.distance import cdist
 from sklearn.cluster import KMeans
-
-
+import numpy as np
 
 def readData():
     df = pd.read_csv("ClusterPlot.csv")
@@ -20,31 +19,22 @@ def readData():
     return df.drop(df.columns[0], axis=1)
 
 
-def elbowMethod(data):
-    #Transforming data values in range of 0<=x<=1 , 0<=y<=1
-    mms=MinMaxScaler()
-    mms.fit(data)
-    data_transformed=mms.transform(data)
-
-    #Converting  data_transformed variable to pandas dataframe
-    data_transformed=pd.DataFrame(data_transformed, columns=['V1', 'V2'])
-    
-    #Elbow method to minimize WSS (Within-cluster Sum of Square)
-    Sum_of_squared_distances = []
-    K = range(1,15)
+def elbowMethod(data):    
+    distortions = []
+    K = range(1,10)
     for k in K:
-        km = KMeans(n_clusters=k)
-        km.fit(data_transformed)
-        Sum_of_squared_distances.append(km.inertia_)
-        
-        
+        model = KMeans(n_clusters=k).fit(data)
+        model.fit(data)
+        distortions.append(sum(np.min(cdist(data, model.cluster_centers_, 'euclidean'), axis=1)) / data.shape[0])
+    
     #Plotting the Elbow curve
-    plt.plot(K, Sum_of_squared_distances, 'bx-')
+    plt.plot(K, distortions, 'bx-')
     plt.xlabel('k')
     plt.ylabel('Sum_of_squared_distances')
     plt.title('Elbow Method For Optimal k')
     plt.show()
-    
+
+
 
 def getColor(i):
     if(i == 0):
@@ -61,7 +51,7 @@ def clustering(data):
     kmeans.fit(data)  #fit kmeans object to dataset
     
     #Identifying the clusters
-    clusters = kmeans.cluster_centers_
+    centroids = kmeans.cluster_centers_
     
     #positions hold values of either 0,1,2 for each point in graph. 0,1,2 corresponds to their respective centroid.
     #These values are used to determine which point belong to which cluster.
@@ -70,8 +60,11 @@ def clustering(data):
     
     for i in range(number_of_clusters):
         plt.scatter(data.values[positions == i,0], data.values[positions == i,1], s=20, color=getColor(i))
-        plt.scatter(clusters[i][0], clusters[i][1], marker='o', s=100, color='black')
+        plt.scatter(centroids[i][0], centroids[i][1], marker='o', s=100, color='black')
         
+    plt.xlabel('V1')
+    plt.ylabel('V2')
+    plt.title('Clustering')
     plt.show()
 
         
